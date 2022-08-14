9899f51e2c05cdacaad143021cc3998c39ccdf54
Nenad Petkovic <root@nenad-petkovic.com>
2022-08-14 19:23:08 +0200

    test: fix coverage report collection

--- packages/ioc/jest.config.ts
+++ packages/ioc/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
     displayName: 'ioc',
-    preset: '../../jest.preset.js',
+    // preset: '../../jest.preset.js',
     globals: {
         'ts-jest': {
             tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -12,5 +12,5 @@ export default {
         '^.+\\.[tj]sx?$': 'ts-jest',
     },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-    coverageDirectory: '../../coverage/packages/core',
+    coverageDirectory: '../../coverage/packages/ioc',
 };
